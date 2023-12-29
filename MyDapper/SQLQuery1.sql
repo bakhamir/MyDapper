d61514c2b0b4ebaad0f4f3351386a7f320d66b61
Amir <Amir@WIN-S7KG9HKT0QA>
2023-12-29 23:49:28 +0600

    *зафиксил*

---
+++ MyDapper/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE pUsers3
+    @id_out INT OUTPUT,
+    @id INT,
+    @last_name NVARCHAR(255),
+    @category_id INT,
+    @IIN NVARCHAR(255)
+AS
+BEGIN
+    UPDATE Users
+    SET last_name = @last_name, category_id = @category_id, IIN = @IIN
+    WHERE Id = @id;
+
+    SET @id_out = @id;
+END;
