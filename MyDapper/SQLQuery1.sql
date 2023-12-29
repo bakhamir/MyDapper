f7e40b17db6104b23124941d2bbe3f1952937a31
Amir <Amir@WIN-S7KG9HKT0QA>
2023-12-29 23:49:37 +0600

    фыв

--- MyDapper/SQLQuery1.sql
+++ MyDapper/SQLQuery1.sql
@@ -12,3 +12,24 @@ BEGIN
 
     SET @id_out = @id;
 END;
+
+CREATE PROCEDURE pUsers
+    @id INT
+AS
+BEGIN
+    SELECT *
+    FROM Users
+    WHERE Id = @id OR @id IS NULL;
+END;
+CREATE PROCEDURE pUsers2
+    @id INT OUTPUT,
+    @last_name NVARCHAR(255),
+    @category_id INT,
+    @IIN NVARCHAR(255)
+AS
+BEGIN
+    INSERT INTO Users (last_name, category_id, IIN)
+    VALUES (@last_name, @category_id, @IIN);
+
+    SET @id = SCOPE_IDENTITY();
+END;
